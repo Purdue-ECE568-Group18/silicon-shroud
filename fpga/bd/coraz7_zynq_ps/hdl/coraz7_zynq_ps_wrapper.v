8c168a5ee4434650c266cb94bbd4cdab70334a0f
jackfrye <jack.frye@comcast.net>
2026-04-30 23:18:11 -0400

    Somehow one of the modules in the block design did not have it's reset pin tied

--- fpga/bd/coraz7_zynq_ps/hdl/coraz7_zynq_ps_wrapper.v
+++ fpga/bd/coraz7_zynq_ps/hdl/coraz7_zynq_ps_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.2 (lin64) Build 6299465 Fri Nov 14 12:34:56 MST 2025
-//Date        : Tue Apr 28 21:53:33 2026
+//Date        : Thu Apr 30 23:09:00 2026
 //Host        : jfrye-B550-VH-B running 64-bit Ubuntu 22.04.5 LTS
 //Command     : generate_target coraz7_zynq_ps_wrapper.bd
 //Design      : coraz7_zynq_ps_wrapper
